8a1ee328700ddf0f7bf812a31101d976c43afdf4
Mariano Hernandez Ramos <mariano.hernandez@softtek.com>
2023-09-29 18:29:38 -0600

    Cambios a estructura

--- FRIDAGPT/__init__.py
+++ FRIDAGPT/__init__.py
@@ -6,23 +6,20 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info("Python HTTP trigger function processed a request.")
-
+    x = json.loads(req.get_body())
+    url_p = x["Goto"]
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=False, slow_mo=50)
         page = browser.new_page()
-
-        url = "https://tigyog.app/d/H7XOvXvC_x/r/goedel-s-first-incompleteness-theorem"
+        url = url_p
         page.goto(url)
-
         title = page.evaluate("() => document.title")
         links = page.evaluate(
             "var links = [];const refs = document.getElementsByTagName('a');for (const c of refs) {links.push(c.href);} uniq = [...new Set(links)]; uniq;"
         )
         content = page.evaluate(
-            "const tags = document.getElementsByTagName('*');const text = [];for (const c of tags) {if(c.innerText){text.push(c.innerText.replace(/(\\r\\n|\\n|\\r)/gm, ''));}}uniq = [...new Set(text)];uniq;"
+            "const tags = document.getElementsByTagName('*');const text = [];for (const c of tags) {if(c.innerText){text.push(c.innerText.replace(/(\\r\\n|\\n|\\r|\\t)/gm, ''));}}uniq = [...new Set(text)];uniq;"
         )
         obj = {"title": title, "links": links, "content": content}
-
         browser.close()
-
         return func.HttpResponse(json.dumps(obj), mimetype="application/json")
